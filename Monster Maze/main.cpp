ed145b47a9716a7d9273ceab80ec2e2352de3b17
rschwartz1223 <rschwartz1223@gmail.com>
2019-07-16 09:09:06 -0700

    Initial Commit

---
+++ Monster Maze/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Monster Maze
+//
+//  Created by Robert Schwartz on 7/16/19.
+//  Copyright © 2019 Robert Schwartz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
